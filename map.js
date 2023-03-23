62fff8736d05c4900119d5aaa4637e0853dd0355
tleegaard <tleegaard@gmail.com>
2023-03-23 16:53:21 +0100

    zoom

--- map.js
+++ map.js
@@ -7,7 +7,7 @@ let mymap = L.map('map', {
    //maxBoundsViscosity: 1.0,
    //minZoom: 13, // set minZoom to the same value as the initial zoom
    //maxZoom: 60 // set maxZoom to the same value as the initial zoom
-}).setView([56.4534, 9.4029], 13);
+}).setView([56.4534, 9.4029], 20); //13
 
 
 const dftoken = '3ebc3a63849a43b46feb8203ab25f83c';

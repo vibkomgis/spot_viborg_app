dca28b10a5073fa49de04407eb8e8ab8099b3b1b
Christian Mathiesen <120325417+ChristianMathiesen@users.noreply.github.com>
2023-03-27 13:46:57 +0200

    Udgået script

--- src/fetchSevaerdigheder.js
+++
@@ -1,9 +0,0 @@
-fetch('data/sevaerdighederData/da-long.json')
-  .then(response => response.json())
-  .then(data => {
-    const longdescription = [];
-    data.forEach(poi => {
-      longdescrip.push(poi.LONG_DESCRIPTION);
-    });
-    console.log(longdescription);
-  });

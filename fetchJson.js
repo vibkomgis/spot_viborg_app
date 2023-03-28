826176c2d1ef108fb60dd5cd68d07968717ba1e1
Christian Mathiesen <120325417+ChristianMathiesen@users.noreply.github.com>
2023-03-28 08:12:24 +0200

    facilities på en/de

--- fetchJson.js
+++ fetchJson.js
@@ -1,21 +1,25 @@
 
 let fetchData = 'data/sevaerdighederData/da-short.json';
-
+let fetchFacilities = 'data/facilities/facilities-da.json'
 function setFetchData(aa) {
     
 
     if (aa === 'en') {
         fetchData = 'data/sevaerdighederData/en-short.json';
+        fetchFacilities = 'data/facilities/facilities-en.json'
     }
     else if (aa === 'de') {
         fetchData = 'data/sevaerdighederData/de-short.json';
+        fetchFacilities = 'data/facilities/facilities-de.json'
     }
     else {
         fetchData = 'data/sevaerdighederData/da-short.json';
+        fetchFacilities = 'data/facilities/facilities-da.json'
     }
 
     const event = new Event('fetchDataUpdated');
     window.dispatchEvent(event);
 
     return fetchData;
+    return fetchFacilities
 }

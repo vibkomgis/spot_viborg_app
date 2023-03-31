7d5dd83a754af2421f5b86dbb5c0de8afc8913e0
Christian Mathiesen <120325417+ChristianMathiesen@users.noreply.github.com>
2023-03-31 10:36:40 +0200

    deaktiver basemap

--- map.js
+++ map.js
@@ -18,7 +18,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(mymap);
 */
 
-/*
+
 
 const toposkaermkortwms = L.tileLayer.wms('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
@@ -44,10 +44,10 @@ const baseLayers = {
 
 L.control.layers(baseLayers).addTo(mymap);
 
-*/
 
 
 
+/*
 var basemaps = [
   L.tileLayer.wms('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: myAttributionText,
@@ -72,7 +72,7 @@ tileX: 0,  // tile X coordinate
 tileY: 0,  // tile Y coordinate
 tileZ: 1   // tile zoom level
 }));
-
+*/
 
 
 
